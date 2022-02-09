88ea62a7766c175458b8084bcd601a4de90181c2
Marcos Albuquerque <mpdsa20@gmail.com>
2022-02-08 22:03:13 -0300

    Fix req 3, req 4 ok

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,37 @@
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 import csv
+import json
+import xmltodict
 
 
 class Inventory:
     def import_data(path, type):
-        with open(path) as file:
-            csv_file = list(csv.DictReader(file))
+        name_file = path.split('.')
+        # print(name_file[1])  # extensão do arquivo
 
-            if(type == 'simples'):
-                return SimpleReport.generate(csv_file)
-            if(type == 'completo'):
-                return CompleteReport.generate(csv_file)
+        with open(path) as file:
+            if(name_file[1] == 'csv'):
+                csv_file = list(csv.DictReader(file))
+
+                if(type == 'simples'):
+                    return SimpleReport.generate(csv_file)
+                if(type == 'completo'):
+                    return CompleteReport.generate(csv_file)
+
+            elif(name_file[1] == 'json'):
+                json_file = json.loads(file.read())
+
+                if(type == 'simples'):
+                    return SimpleReport.generate(json_file)
+                if(type == 'completo'):
+                    return CompleteReport.generate(json_file)
+
+            elif(name_file[1] == 'xml'):
+                xml_file = xmltodict.parse(file.read())
+                # xml_file = list(dict(file.getroot()))
+
+                if(type == 'simples'):
+                    return SimpleReport.generate(xml_file)
+                if(type == 'completo'):
+                    return CompleteReport.generate(xml_file)
