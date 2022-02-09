16649814be07b81b95a800897832e6e5e81b859c
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-02-09 19:04:47 -0300

    fix all requirements

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,20 +1,20 @@
-# from inventory_report.importer.importer import Importer
-# import xml.etree.ElementTree as ET
+from inventory_report.importer.importer import Importer
+import xml.etree.ElementTree as ET
 
 
-# class XmlImporter(Importer):
-#     @classmethod
-#     def import_data(cls, path):
-#         if path.endswith(".xml"):
-#             list = []
-#             tree = ET.parse(path)
-#             result = tree.getroot()
+class XmlImporter(Importer):
+    @classmethod
+    def import_data(cls, path):
+        if path.endswith(".xml"):
+            list = []
+            tree = ET.parse(path)
+            result = tree.getroot()
 
-#             for item in result:
-#                 list_of_objects = {}
-#                 for item in item:
-#                     list_of_objects[item.tag] = item.text
-#                 list.append(list_of_objects)
-#                 return list
-#         else:
-#             raise ValueError("Arquivo inválido")
+            for item in result:
+                list_of_objects = {}
+                for item in item:
+                    list_of_objects[item.tag] = item.text
+                list.append(list_of_objects)
+                return list
+        else:
+            raise ValueError("Arquivo inválido")
